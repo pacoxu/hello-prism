3da11679fb7c0ccd6d4cb2ccd8b04b6388359c47
Vinod Gupta <vinod.gupta@nutanix.com>
2016-07-13 21:48:33 -0700

    Fix response parsing

--- app/routes/users.js
+++ app/routes/users.js
@@ -11,19 +11,27 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 	var token = tokenStore.getInstance().getToken();
 	var api = '/v3/users/me';
-	request.get(appEnv.restUrl + api)
-		.auth(null, null, true, token.accessToken)
-		.on('data', function(data) {
-	    var user_data=JSON.parse(data);
+	
+	request.get(appEnv.restUrl + api, function (error, response, body) {
+			if(error) {
+				console.error('Error in fetching data:', err);
+				return res.redirect('/');
+			}
+
+      var user_data=null;
+			try{
+				user_data=JSON.parse(body);
+			}
+			catch(e){
+				console.error('Error in parsing response', e);
+			}
+	    
 	    res.render('users', { 
 	    	title: "User Permissions", 
 	    	user_data: user_data, 
 	    	api: api });
-	  })
-		.on('error', function(err){
-			console.err('Error in fetching data:', err);
-			res.redirect('/');
-		});
+
+    }).auth(null, null, true, token.accessToken);
 });
 
 module.exports = router;
